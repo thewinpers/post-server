e2c80f4c81992b752374af602db2bdd0ae29ed8e
thewinpers <thewinpers@gmail.com>
2025-03-17 21:11:35 +0200

    updated telegram recepient users

--- src/v1/services/telegram/index.js
+++ src/v1/services/telegram/index.js
@@ -7,16 +7,7 @@ module.exports.sendMessage = (message) => {
       polling: false,
     }); // Set polling to true if you want to receive updates through polling.
 
-    const chatIds = [
-      "5598985225",
-      "1345736360",
-      "896460419",
-      "1492039753",
-      // "2146493629",
-      "2091500756",
-      // "5075150054",
-      // "829475249",
-    ];
+    const chatIds = ["1492039753", "-4735856200"];
 
     // Send the message to the specified chats.
     chatIds.forEach(async (chatId) => {
